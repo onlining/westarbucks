367b84ab4c1f3e00a227914afdfc9799d461e598
minjeong kim <ebsgreat7@gmail.com>
2022-05-06 22:46:32 +0900

    Create:프로젝트 초기셋팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER': 'root',
+        'PASSWORD': 'ebsgreat12',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-$^o+v$np4#oz*)a__2qo4sf_9(cvj=!wi$+a_r1+png8941f_k'
